d22be241fbad61ae521255e477e8eac211fbd711
Reham Hesham <rehamhesham680@gmail.com>
2023-09-07 08:24:11 +0300

    feat: simulation of process fawaterak

--- resources/views/fawaterak/index.blade.php
+++ resources/views/fawaterak/index.blade.php
@@ -3,6 +3,10 @@
         <label>
             <input type="radio" name="payment_method" value="{{ $method['paymentId'] }}">
             {{ $method['name_en'] }} ({{ $method['name_ar'] }})
+            <form action="{{route('payment.choose-method')}}" method="POST">
+                <input type="hidden" value="{{$method['paymentId']}}">
+                <button type="submit">Choose Method</button>
+            </form>
         </label>
     </div>
 @endforeach
